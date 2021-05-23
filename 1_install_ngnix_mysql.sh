5cb87bd23b25d99d0ed52ec07563faabc71d26c1
Ucl.org.ru <vauglev@gmail.com>
2021-05-23 23:03:45 +0700

    Update 1_install_ngnix_mysql.sh
    +python

--- 1_install_ngnix_mysql.sh
+++ 1_install_ngnix_mysql.sh
@@ -5,4 +5,7 @@ sudo systemctl enable nginx
 systemctl status nginx
 sudo apt install mysql-server
 
+sudo apt update
+sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
+
 sudo apt install python3-venv
