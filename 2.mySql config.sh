8677ab676b9de284ece8fa6c7b0f842558064a87
Ucl.org.ru <vauglev@gmail.com>
2021-05-23 23:14:57 +0700

    Create 2.mySql config.sh
    Настройка рутт - способ подключения 
    + создат узер yg

---
+++ 2.mySql config.sh
@@ -0,0 +1,9 @@
+
+echo " SELECT user,authentication_string,plugin,host FROM mysql.user;sudo mysql"
+echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '123456';"
+echo "FLUSH PRIVILEGES;"
+echo "SELECT user,authentication_string,plugin,host FROM mysql.user;"
+echo "CREATE USER 'yg'@'localhost' IDENTIFIED BY '123456';"
+echo "GRANT ALL PRIVILEGES ON *.* TO 'yg'@'localhost' WITH GRANT OPTION;"
+echo "exit"
+sudo mysql
